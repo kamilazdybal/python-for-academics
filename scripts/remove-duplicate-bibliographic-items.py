114e44b0a27f95c2bc2bb1ac6d19c81e80a74e96
kamilazdybal <kamilazdybal@gmail.com>
2023-07-12 11:07:22 +0200

    Code update

---
+++ scripts/remove-duplicate-bibliographic-items.py
@@ -0,0 +1,44 @@
+import re
+
+directory = './'
+filename = 'bibliography.bib'
+
+file = open(directory + filename, 'r')
+file_content = file.read()
+file_list = file_content.split('\n')
+
+tags_list = []
+
+for item in file_list:
+    
+    match = re.search(r'@.*\{(.*),', item)
+    
+    if match is not None:
+        
+        tag = match.group(1)
+        tags_list.append(tag)
+
+file.close()
+
+unique_tags = list(set(tags_list))
+
+new_file = open(directory + 'unique-bibliography.bib', 'w')
+
+bib_items = file_content.split('@')[1::]
+bib_items = ['@' + item for item in bib_items]
+
+for item in bib_items:
+
+    match = re.search(r'@.*\{(.*),', item)
+    
+    if match is not None:
+        
+        tag = match.group(1)
+
+    if tag in unique_tags:
+
+        new_file.write(item)
+
+        unique_tags.remove(tag)
+
+new_file.close()
