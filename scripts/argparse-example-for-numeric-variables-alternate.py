59fce075af1206f7a7274a23806716001429d1f2
kamilazdybal <kamilazdybal@gmail.com>
2024-03-29 15:20:03 +0100

    Added scripts

---
+++ scripts/argparse-example-for-numeric-variables-alternate.py
@@ -0,0 +1,37 @@
+import argparse
+import numpy as np
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Case settings
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# Set parameters:
+input_variables = [0, 1, 2, 3, 4]
+n_epochs = 1000
+learning_rate = 0.001
+initializer = 'GlorotUniform'
+min_random_seed, max_random_seed = (0,10)
+
+# List of random seeds to loop over:
+random_seeds_list = [i for i in range(min_random_seed, max_random_seed)]
+
+# Names of input variables:
+input_names = np.array(['X' + str(i+1) for i in range(0,10)])
+
+# Create a tag for this training session:
+case_run_name = 'inputs-' + '-'.join(input_names[input_variables]) + '-n-epochs-' + str(n_epochs) + '-lr-' + str(learning_rate) + '-initializer-' + initializer
+
+print(case_run_name)
+
+print()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Neural network training with the current parameters starts here...
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# ...
+
+# ...
+
+# ...
+
---
+++ scripts/argparse-example-for-numeric-variables.py
@@ -0,0 +1,78 @@
+import argparse
+import numpy as np
+
+def argument_parser():
+
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--input_variables',
+                        type=int,
+                        default=[0,1,2,3,4],
+                        nargs="+",
+                        help='Indices for the input variables')
+    
+    parser.add_argument('--n_epochs',
+                        type=int,
+                        default=1000,
+                        help='Number of epochs')
+    
+    parser.add_argument('--learning_rate',
+                        type=float,
+                        default=0.001,
+                        help='Learning rate')
+    
+    parser.add_argument('--initializer',
+                        type=str,
+                        default='GlorotUniform',
+                        help='Initialization of weights')
+    
+    parser.add_argument('--random_seeds',
+                        type=int,
+                        default=[0,10],
+                        nargs="+",
+                        help='Min and max random seed')
+    
+    return parser
+
+parser = argument_parser()
+
+args = parser.parse_args()
+
+print(args)
+
+print()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Case settings
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# Populate parameter values from argument parser:
+input_variables = vars(args).get('input_variables')
+n_epochs = vars(args).get('n_epochs')
+learning_rate = vars(args).get('learning_rate')
+initializer = vars(args).get('initializer')
+min_random_seed, max_random_seed = tuple(vars(args).get('random_seeds'))
+
+# List of random seeds to loop over:
+random_seeds_list = [i for i in range(min_random_seed, max_random_seed)]
+
+# Names of input variables:
+input_names = np.array(['X' + str(i+1) for i in range(0,10)])
+
+# Create a tag for this training session:
+case_run_name = 'inputs-' + '-'.join(input_names[input_variables]) + '-n-epochs-' + str(n_epochs) + '-lr-' + str(learning_rate) + '-initializer-' + initializer
+
+print(case_run_name)
+
+print()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Neural network training with the current parameters starts here...
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# ...
+
+# ...
+
+# ...
+
